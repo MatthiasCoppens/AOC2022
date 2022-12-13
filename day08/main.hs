68a37f943cd0c5536bb9986ecf56fb399d02cf8b
Matthias Coppens <coppens.matthias.abc@gmail.com>
2022-12-13 13:38:04 +0100

    Day 8

--- day08/main.hs
+++ day08/main.hs
@@ -1,5 +1,13 @@
 import Data.List
 
+takeUntil :: (a -> Bool) -> [a] -> [a]
+takeUntil f = go
+    where
+        go (x : xs)
+            | f x       = [x]
+            | otherwise = x : go xs
+        go [] = []
+
 grid :: String -> [[Int]]
 grid = (map . map) (subtract (fromEnum '0') . fromEnum) . lines
 
@@ -13,7 +21,22 @@ visible g = foldl1 ((zipWith . zipWith) (||)) [left g, right g, above g, below g
 solve1 :: [[Int]] -> Int
 solve1 = length . filter id . concat . visible
 
+lefts, rights, aboves, belows :: [[Int]] -> [[[Int]]]
+lefts  = map $ map reverse . inits
+rights = map $ tail        . tails
+aboves = transpose . lefts  . transpose
+belows = transpose . rights . transpose
+
+treesInDirection :: [[Int]] -> ([[Int]] -> [[[Int]]]) -> [[Int]]
+treesInDirection g trees = (zipWith . zipWith) f g $ trees g
+    where
+        f n ns = length $ takeUntil (>= n) ns
+
+solve2 :: [[Int]] -> Int
+solve2 g = maximum $ concat $ foldl1 ((zipWith . zipWith) (*)) $ map (treesInDirection g) [lefts, rights, aboves, belows]
+
 main :: IO ()
 main = do
     input <- grid <$> getContents
     print $ solve1 input
+    print $ solve2 input
